5a928a47c3383eb101b3793395c0732b6657c30d
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-26 15:26:08 -0600

    complete step 5

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
-  }
+    super();
+
+    this.state = {
+      students: [],
+    };
+  };
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3005/students?class=${this.props.match.params.class}`)
+      .then(res => {
+        this.setState({
+          students: res.data,
+        });
+      });
+  };
 
   render() {
+    console.log(this.props)
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {this.state.students.map((student, i) => (
+          <h3 key={i}>{student.first_name} {student.last_name}</h3>
+        ))}
       </div>
-    )
-  }
-}
+    );
+  };
+};
